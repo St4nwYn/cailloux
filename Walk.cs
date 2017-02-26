b3bff21ee66c653e67c7799d99c8e203ee69a6c9
St4nwYn <st4nwYn@gmail.com>
2017-02-26 18:08:17 +0100

    Add files via upload

--- Walk.cs
+++ Walk.cs
@@ -1,41 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Timers;
 
 public class Walk : MonoBehaviour
 {
-    public float speed;
-	// Use this for initialization
-	void Start ()
+    CharacterController CharacterCollider;
+    public float walkSpeed = 0.0666f;
+    public float runSpeed = 0.1333f;
+    public float crouchSpeed = 0.05f;
+    float dist;
+    // Use this for initialization
+    void Start()
     {
-        speed = 0.1f;
+        CharacterCollider = gameObject.GetComponent<CharacterController>();
+        //Prevent the player from falling  
+        this.GetComponent<Rigidbody>().freezeRotation = true;
+
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         Movement();
-	}
+    }
 
-    //Permise movements of the player with the different buttons
+
+    //check the state of the character (up/down, running/walking)
+    bool isCrouching = false;
+    bool isRunning = false;
+
     void Movement()
     {
-        Timer timer = new Timer(1000);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            this.transform.Translate(Vector3.left * speed);
-        if (Input.GetKey(KeyCode.RightArrow))
-            this.transform.Translate(Vector3.right * speed);
-        if (Input.GetKey(KeyCode.UpArrow))
-            this.transform.Translate(Vector3.forward * speed);
-        if (Input.GetKey(KeyCode.DownArrow))
-            this.transform.Translate(Vector3.back * speed);
-        /*if (Input.GetKey(KeyCode.Space))
-        {
-            this.transform.Translate(Vector3.up * speed);
-            timer.Enabled = true;
-            timer.Start();
-            this.transform.Translate(Vector3.down * speed);
-        }*/
+        //determine the speed in depending on the movement (running, walking, crouching)
+        float speed = walkSpeed;
+        if (isRunning)
+            speed = runSpeed;
+        else if (isCrouching)
+            speed = crouchSpeed;
 
+        //Permise movements of the player with the different buttons
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+            this.transform.Translate(Vector3.left * speed);
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            this.transform.Translate(Vector3.right * speed);
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
+            this.transform.Translate(Vector3.forward * speed);
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            this.transform.Translate(Vector3.back * speed);
+        if (Input.GetKey(KeyCode.Space))
+            this.transform.Translate(Vector3.up * speed);
+
+        if (Input.GetKey(KeyCode.LeftShift) && !isCrouching)
+            isRunning = true;
+        else
+            isRunning = false;
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isCrouching && !isRunning)
+        {
+            isCrouching = true;
+            //this.CharacterCollider.height = 0.5f;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl) && isCrouching)
+        {
+            isCrouching = false;
+            //this.CharacterCollider.height = 1.0f;
+        }
     }
 }
